a216150cf333f6baac32471f391575ffc1bfd937
Rubilmax <rmilon@gmail.com>
2025-05-06 16:59:32 +0200

    test(trace): update snapshots

--- test/traceCall.test.ts
+++ test/traceCall.test.ts
@@ -57,8 +57,8 @@ describe("traceCall", () => {
       [ContractFunctionExecutionError: execution reverted: revert: ERC20: transfer amount exceeds balance
 
       0 ↳ FROM 0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266
-      0 ↳ CALL (0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48).transfer(0xf39Fd6…0xf3, 100000000) -> ERC20: transfer amount exceeds balance
-        1 ↳ DELEGATECALL (0x43506849d7c04f9138d1a2050bbf3a0c054402dd).transfer(0xf39Fd6…0xf3, 100000000) -> ERC20: transfer amount exceeds balance
+      0 ↳ CALL (0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48).transfer(0xf39Fd6e5…2266, 100000000) -> ERC20: transfer amount exceeds balance
+        1 ↳ DELEGATECALL (0x43506849d7c04f9138d1a2050bbf3a0c054402dd).transfer(0xf39Fd6e5…2266, 100000000) -> ERC20: transfer amount exceeds balance
 
        
       Estimate Gas Arguments:
@@ -81,7 +81,7 @@ describe("traceCall", () => {
         sender:    0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
 
       Docs: https://viem.sh/docs/contract/writeContract
-      Version: viem@2.22.2]
+      Version: viem@2.29.0]
     `);
   });
 
@@ -104,11 +104,11 @@ describe("traceCall", () => {
       [ExecutionRevertedError: ERC20: transfer from the zero address
 
       0 ↳ FROM 0x0000000000000000000000000000000000000000
-      0 ↳ CALL (0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48).transfer(0xf39Fd6…0xf3, 100000000) -> ERC20: transfer from the zero address
-        1 ↳ DELEGATECALL (0x43506849d7c04f9138d1a2050bbf3a0c054402dd).transfer(0xf39Fd6…0xf3, 100000000) -> ERC20: transfer from the zero address
+      0 ↳ CALL (0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48).transfer(0xf39Fd6e5…2266, 100000000) -> ERC20: transfer from the zero address
+        1 ↳ DELEGATECALL (0x43506849d7c04f9138d1a2050bbf3a0c054402dd).transfer(0xf39Fd6e5…2266, 100000000) -> ERC20: transfer from the zero address
 
 
-      Version: viem@2.22.2]
+      Version: viem@2.29.0]
     `);
   });
 
@@ -133,7 +133,7 @@ describe("traceCall", () => {
         sender:    0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
 
       Docs: https://viem.sh/docs/contract/writeContract
-      Version: viem@2.22.2]
+      Version: viem@2.29.0]
     `);
 
     client.transport.tracer.failed = true;
@@ -149,8 +149,8 @@ describe("traceCall", () => {
       [ContractFunctionExecutionError: execution reverted: revert: ERC20: transfer amount exceeds balance
 
       0 ↳ FROM 0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266
-      0 ↳ CALL (0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48).transfer(0xf39Fd6…0xf3, 100000000) -> ERC20: transfer amount exceeds balance
-        1 ↳ DELEGATECALL (0x43506849d7c04f9138d1a2050bbf3a0c054402dd).transfer(0xf39Fd6…0xf3, 100000000) -> ERC20: transfer amount exceeds balance
+      0 ↳ CALL (0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48).transfer(0xf39Fd6e5…2266, 100000000) -> ERC20: transfer amount exceeds balance
+        1 ↳ DELEGATECALL (0x43506849d7c04f9138d1a2050bbf3a0c054402dd).transfer(0xf39Fd6e5…2266, 100000000) -> ERC20: transfer amount exceeds balance
 
        
       Estimate Gas Arguments:
@@ -173,7 +173,7 @@ describe("traceCall", () => {
         sender:    0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
 
       Docs: https://viem.sh/docs/contract/writeContract
-      Version: viem@2.22.2]
+      Version: viem@2.29.0]
     `);
   });
 
@@ -199,9 +199,9 @@ describe("traceCall", () => {
       [
         [
           "0 ↳ FROM 0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266
-      0 ↳ CALL (0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48).transfer[ 37,560 / 29,978,392 ](0xf39Fd6…0xf3, 50000000) -> 0x0000000000000000000000000000000000000000000000000000000000000001
-        1 ↳ DELEGATECALL (0x43506849d7c04f9138d1a2050bbf3a0c054402dd).transfer[ 11,463 / 29,502,848 ](0xf39Fd6…0xf3, 50000000) -> 0x0000000000000000000000000000000000000000000000000000000000000001
-          2 ↳ LOG Transfer(0xf39Fd6…0xf3, 0xf39Fd6…0xf3, 50000000)
+      0 ↳ CALL (0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48).transfer[ 37,560 / 29,978,392 ](0xf39Fd6e5…2266, 50000000) -> 0x0000000000000000000000000000000000000000000000000000000000000001
+        1 ↳ DELEGATECALL (0x43506849d7c04f9138d1a2050bbf3a0c054402dd).transfer[ 11,463 / 29,502,848 ](0xf39Fd6e5…2266, 50000000) -> 0x0000000000000000000000000000000000000000000000000000000000000001
+          2 ↳ LOG Transfer(0xf39Fd6e5…2266, 0xf39Fd6e5…2266, 50000000)
       ",
         ],
       ]
@@ -231,7 +231,7 @@ describe("traceCall", () => {
         sender:    0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
 
       Docs: https://viem.sh/docs/contract/writeContract
-      Version: viem@2.22.2]
+      Version: viem@2.29.0]
     `);
 
     await expect(
@@ -245,8 +245,8 @@ describe("traceCall", () => {
       [ContractFunctionExecutionError: execution reverted: revert: ERC20: transfer amount exceeds balance
 
       0 ↳ FROM 0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266
-      0 ↳ CALL (0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48).transfer(0xf39Fd6…0xf3, 100000000) -> ERC20: transfer amount exceeds balance
-        1 ↳ DELEGATECALL (0x43506849d7c04f9138d1a2050bbf3a0c054402dd).transfer(0xf39Fd6…0xf3, 100000000) -> ERC20: transfer amount exceeds balance
+      0 ↳ CALL (0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48).transfer(0xf39Fd6e5…2266, 100000000) -> ERC20: transfer amount exceeds balance
+        1 ↳ DELEGATECALL (0x43506849d7c04f9138d1a2050bbf3a0c054402dd).transfer(0xf39Fd6e5…2266, 100000000) -> ERC20: transfer amount exceeds balance
 
        
       Estimate Gas Arguments:
@@ -269,7 +269,7 @@ describe("traceCall", () => {
         sender:    0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
 
       Docs: https://viem.sh/docs/contract/writeContract
-      Version: viem@2.22.2]
+      Version: viem@2.29.0]
     `);
   });
 
@@ -287,9 +287,9 @@ describe("traceCall", () => {
       [ContractFunctionExecutionError: execution reverted: revert: ERC20: transfer amount exceeds balance
 
       0 ↳ FROM 0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266
-      0 ↳ CALL (0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48).transfer(0xf39Fd6…0xf3, 100000000) -> ERC20: transfer amount exceeds balance
+      0 ↳ CALL (0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48).transfer(0xf39Fd6e5…2266, 100000000) -> ERC20: transfer amount exceeds balance
       {"from":"0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266","gas":"0x1c96f24","gasUsed":"0x85d9","to":"0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48","input":"0xa9059cbb000000000000000000000000f39fd6e51aad88f6f4ce6ab8827279cfffb922660000000000000000000000000000000000000000000000000000000005f5e100","output":"0x08c379a00000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000002645524332303a207472616e7366657220616d6f756e7420657863656564732062616c616e63650000000000000000000000000000000000000000000000000000","error":"execution reverted","revertReason":"ERC20: transfer amount exceeds balance","calls":[{"from":"0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48","gas":"0x1c22d8c","gasUsed":"0x14f1","to":"0x43506849d7c04f9138d1a2050bbf3a0c054402dd","input":"0xa9059cbb000000000000000000000000f39fd6e51aad88f6f4ce6ab8827279cfffb922660000000000000000000000000000000000000000000000000000000005f5e100","output":"0x08c379a00000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000002645524332303a207472616e7366657220616d6f756e7420657863656564732062616c616e63650000000000000000000000000000000000000000000000000000","error":"execution reverted","revertReason":"ERC20: transfer amount exceeds balance","value":"0x0","type":"DELEGATECALL"}],"value":"0x0","type":"CALL"}
-        1 ↳ DELEGATECALL (0x43506849d7c04f9138d1a2050bbf3a0c054402dd).transfer(0xf39Fd6…0xf3, 100000000) -> ERC20: transfer amount exceeds balance
+        1 ↳ DELEGATECALL (0x43506849d7c04f9138d1a2050bbf3a0c054402dd).transfer(0xf39Fd6e5…2266, 100000000) -> ERC20: transfer amount exceeds balance
       {"from":"0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48","gas":"0x1c22d8c","gasUsed":"0x14f1","to":"0x43506849d7c04f9138d1a2050bbf3a0c054402dd","input":"0xa9059cbb000000000000000000000000f39fd6e51aad88f6f4ce6ab8827279cfffb922660000000000000000000000000000000000000000000000000000000005f5e100","output":"0x08c379a00000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000002645524332303a207472616e7366657220616d6f756e7420657863656564732062616c616e63650000000000000000000000000000000000000000000000000000","error":"execution reverted","revertReason":"ERC20: transfer amount exceeds balance","value":"0x0","type":"DELEGATECALL"}
 
        
@@ -313,7 +313,7 @@ describe("traceCall", () => {
         sender:    0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
 
       Docs: https://viem.sh/docs/contract/writeContract
-      Version: viem@2.22.2]
+      Version: viem@2.29.0]
     `);
 
     await expect(
@@ -327,9 +327,9 @@ describe("traceCall", () => {
       [ContractFunctionExecutionError: execution reverted: revert: ERC20: transfer amount exceeds balance
 
       0 ↳ FROM 0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266
-      0 ↳ CALL (0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48).transfer(0xf39Fd6…0xf3, 100000000) -> ERC20: transfer amount exceeds balance
+      0 ↳ CALL (0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48).transfer(0xf39Fd6e5…2266, 100000000) -> ERC20: transfer amount exceeds balance
       {"from":"0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266","gas":"0x1c96f24","gasUsed":"0x85d9","to":"0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48","input":"0xa9059cbb000000000000000000000000f39fd6e51aad88f6f4ce6ab8827279cfffb922660000000000000000000000000000000000000000000000000000000005f5e100","output":"0x08c379a00000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000002645524332303a207472616e7366657220616d6f756e7420657863656564732062616c616e63650000000000000000000000000000000000000000000000000000","error":"execution reverted","revertReason":"ERC20: transfer amount exceeds balance","calls":[{"from":"0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48","gas":"0x1c22d8c","gasUsed":"0x14f1","to":"0x43506849d7c04f9138d1a2050bbf3a0c054402dd","input":"0xa9059cbb000000000000000000000000f39fd6e51aad88f6f4ce6ab8827279cfffb922660000000000000000000000000000000000000000000000000000000005f5e100","output":"0x08c379a00000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000002645524332303a207472616e7366657220616d6f756e7420657863656564732062616c616e63650000000000000000000000000000000000000000000000000000","error":"execution reverted","revertReason":"ERC20: transfer amount exceeds balance","value":"0x0","type":"DELEGATECALL"}],"value":"0x0","type":"CALL"}
-        1 ↳ DELEGATECALL (0x43506849d7c04f9138d1a2050bbf3a0c054402dd).transfer(0xf39Fd6…0xf3, 100000000) -> ERC20: transfer amount exceeds balance
+        1 ↳ DELEGATECALL (0x43506849d7c04f9138d1a2050bbf3a0c054402dd).transfer(0xf39Fd6e5…2266, 100000000) -> ERC20: transfer amount exceeds balance
       {"from":"0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48","gas":"0x1c22d8c","gasUsed":"0x14f1","to":"0x43506849d7c04f9138d1a2050bbf3a0c054402dd","input":"0xa9059cbb000000000000000000000000f39fd6e51aad88f6f4ce6ab8827279cfffb922660000000000000000000000000000000000000000000000000000000005f5e100","output":"0x08c379a00000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000002645524332303a207472616e7366657220616d6f756e7420657863656564732062616c616e63650000000000000000000000000000000000000000000000000000","error":"execution reverted","revertReason":"ERC20: transfer amount exceeds balance","value":"0x0","type":"DELEGATECALL"}
 
        
@@ -353,7 +353,7 @@ describe("traceCall", () => {
         sender:    0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
 
       Docs: https://viem.sh/docs/contract/writeContract
-      Version: viem@2.22.2]
+      Version: viem@2.29.0]
     `);
   });
 });
